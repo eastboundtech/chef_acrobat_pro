343da24d3cb41482b3c8b7b293d4892f312a2cf8
hieunba <hi3unb@gmail.com>
2019-03-17 11:28:08 +0700

    Require helpers from windows cookbook

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,8 @@ chef_version '>= 13.0'
 
 supports 'windows'
 
+depends 'windows', '~> 5.2.3'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
